61586c14aaa1a026fe61fdeaf016e1716d789ff5
swethakeerthipati <swethakeerthipati@gmail.com>
2023-06-29 06:49:20 +0200

    Add CSS and more functionality

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,35 @@
+import React from "react";
+import Link from "next/link";
+
 export default function HomePage() {
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <Link href="/volumes">Go to all volumes</Link>
     </div>
   );
 }
+
+// import Link from "next/link";
+// import { introduction } from "../resources/lib/data";
+// // import { useRouter } from "next/router";
+// import { volumes } from "../resources/lib/data";
+
+// export default function HomePage() {
+//   // const router = useRouter();
+//   return (
+//     <div>
+//       <h1>Lord of the Rings</h1>
+//       {/* <Link href="/Volumes">GO to VOlumes.</Link> * juts put this in the index.js in volume folder/  } */}
+//       <p>{introduction}</p>
+
+//       {/* <h2>All Volumes</h2> */}
+//       <ul>
+//         {volumes.map((volume) => (
+//           <li key={volume.slug}>
+//             <Link href={`/Volumes/${volume.slug}`}>{volume.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
---
+++ pages/volumes/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { introduction, volumes } from "../../resources/lib/data";
+
+export default function Volumes() {
+  const router = useRouter();
+
+  function redirectToRandomVolume() {
+    const randomIndex = Math.floor(Math.random() * volumes.length);
+    const randomVolumeSlug = volumes[randomIndex].slug;
+    router.push(`/volumes/${randomVolumeSlug}`);
+  }
+
+  return (
+    <>
+      <h1>The Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <button onClick={redirectToRandomVolume}>Surprise Me!</button>
+      <ul>
+        {volumes.map((volume) => (
+          <li key={volume.slug}>
+            <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+// import Link from "next/link";
+// import { useRouter } from "next/router";
+// import { introduction, volumes } from "../../resources/lib/data";
+
+// export default function Volumes() {
+//   const router = useRouter();
+
+//   function redirectToRandomVolume() {
+//     const randomIndex = Math.floor(Math.random() * volumes.length);
+//     const randomVolumeSlug = volumes[randomIndex].slug;
+//   }
+
+//   return (
+//     <>
+//       <h1>The Lord of the Rings</h1>
+//       <p>{introduction}</p>
+//       <button onClick={redirectToRandomVolume}>Suprise Me!</button>
+//       <ul>
+//         {volumes.map((volume) => (
+//           <li key={volume.slug}>
+//             <Link href={`/volumes/${volume.slug}`}>{volume.title}</Link>
+//           </li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
